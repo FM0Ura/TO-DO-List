c2467b1bc6893b1302e8f386a20c99cb23c5a123
Felipe Angelo Zucolotto Moura <fanz.moura@protonmail.com>
2023-11-07 17:50:48 -0300

    CSS and JS imported to base HTML

--- todolist/task/urls.py
+++ todolist/task/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloWorld),
-    path('task-list/', views.taskList, name="task-list"),
-    path('yourname/<str:name>', views.yourname, name="yourname"),
+    path('helloworld/', views.helloWorld),
+    path('', views.taskList, name='task-list'),
+    path('yourname/<str:name>', views.yourname, name='your-name'),
 ]
